56062b415c2ccedcf8a59f7e7d9e14309a61c68d
Husaini Najib <husaininajib98@gmail.com>
2021-10-28 00:04:16 +0800

    working

--- apps.js
+++ apps.js
@@ -1,6 +1,8 @@
 const hamburgerButton = document.querySelector(".nav-toggle")
 const menuList = document.querySelector(".nav-menu")
 const closeButton = document.querySelector(".fa-times")
+const navLink = document.querySelectorAll(".nav-link")
+const navPanel = document.querySelector(".header")
 
 function toggleButton() {
     menuList.classList.toggle("show")
@@ -11,3 +13,20 @@ function navClose() {
     menuList.classList.remove("show")
 }
 closeButton.addEventListener("click", navClose)
+
+navLink.forEach(item => {
+    item.addEventListener("click", navClose)
+})
+
+function displayNavPanel() {
+    if(this.scrollY >= 100) {
+    navPanel.classList.add("scroll-header")
+    } else {
+    navPanel.classList.remove("scroll-header")
+    }
+}
+window.addEventListener("scroll", displayNavPanel )
+
+
+
+
