0b2f388c46118fefac70a34174074b138781a42c
Husaini Najib <husaininajib98@gmail.com>
2021-10-31 20:27:43 +0800

    working

--- apps.js
+++ apps.js
@@ -73,3 +73,44 @@ function scrollDisplay() {
     }
 }
 window.addEventListener("scroll", scrollDisplay)
+
+
+// const sections = document.querySelectorAll("section")
+// const navLi = document.querySelectorAll("nav ul li")
+
+// window.addEventListener("scroll", () => {
+//     let current = "";
+//     sections.forEach(section => {
+//         const sectionTop = section.offsetTop - 50;
+//         const sectionHeight = section.clientHeight;
+//         if(pageYOffset > sectionTop) {
+//             current = section.getAttribute("id")
+//         }
+//     })
+//     navLi.forEach( li => {
+//         li.classList.remove("active-link");
+//         if(li.classList.contains(current)) {
+//             li.classLisst.add("active-link")
+//         }
+//     })
+
+// })
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
